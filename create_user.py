5e2abae09604fbaf3df1ced6070af12b6f77070a
Roberth <marcanoroberth98@gmail.com>
2023-06-23 13:58:52 -0300

    Solving bug while creating user

--- create_user.py
+++ create_user.py
@@ -10,13 +10,15 @@ with open("App/create_tables.sql") as f:
     querys = f.read().split(";")
 
 with app.app_context():
+    db.create_all()
     for query in querys:
         stmt = db.text(query)
         db.session.execute(stmt)
     db.session.commit()
 
 with app.app_context():
-    user_datastore.create_user(email=current_app.config["EMAIL"], 
-                               password=hash_password(current_app.config["PASSWORD"]),
-                              username=current_app.config["USERNAME"])
-    db.session.commit()
+    if not user_datastore.find_user(email="robmago080617@gmail.com"):
+        user_datastore.create_user(email=current_app.config["EMAIL"], 
+                                   password=hash_password(current_app.config["PASSWORD"]),
+                                  username=current_app.config["USERNAME"])
+        db.session.commit()
