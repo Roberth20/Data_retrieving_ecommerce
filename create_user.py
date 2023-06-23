fbd4b933800fd49516e06a80daf312aa14cb3cca
Roberth <marcanoroberth98@gmail.com>
2023-06-23 13:50:08 -0300

    Adding tables init

--- create_user.py
+++ create_user.py
@@ -6,6 +6,15 @@ import App
 
 app = App.create_app()
 
+with open("App/create_tables.sql") as f:
+    querys = f.read().split(";")
+
+with app.app_context():
+    for query in querys:
+        stmt = db.text(query)
+        db.session.execute(stmt)
+    db.session.commit()
+
 with app.app_context():
     user_datastore.create_user(email=current_app.config["EMAIL"], 
                                password=hash_password(current_app.config["PASSWORD"]),
