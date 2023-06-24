6a968c8aebbc2debf9ef884095df679b0bdf431a
Roberth <marcanoroberth98@gmail.com>
2023-06-23 22:31:46 -0300

    Adding webhook endpoint

--- App/main/routes.py
+++ App/main/routes.py
@@ -1,8 +1,16 @@
 from App.main import bp
 from flask import render_template
 from flask_security import auth_required
+from flask import request
 
 @bp.route("/")
 @auth_required("basic")
 def index():
     return render_template("index.html")
+
+@bp.route("/webhook", methods=["POST"])
+def webhook():
+    if request.method == "POST":
+        raw = request.get_json()
+        print("Notificacion recibida: ", raw)
+        return "Webhook received"
