c1e487f86a4943c426d6f1e26633bd6a6a8cc5fe
Roberth <marcanoroberth98@gmail.com>
2023-06-28 11:19:14 -0300

    Better auth_error page and fix encryptation bug

--- App/auth/funcs.py
+++ App/auth/funcs.py
@@ -1,11 +1,11 @@
 from cryptography.fernet import Fernet
 
 def encrypt(data, key):
-    f = Fernet(key)
+    f = Fernet(key.encode())
     encoded = data.encode()
     return f.encrypt(encoded)
 
 def decrypt(encrypted, key):
-    f = Fernet(key)
+    f = Fernet(key.encode())
     decrypted = f.decrypt(encrypted)
     return decrypted.decode()
