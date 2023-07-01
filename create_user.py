1d5655e73f1f250f59287cc0b783bb6726747d3a
Roberth <marcanoroberth98@gmail.com>
2023-07-01 15:04:18 -0300

    r

--- create_user.py
+++ create_user.py
@@ -31,8 +31,8 @@ with app.app_context():
 
 # If the user is not in the database, create it
 with app.app_context():
-    if not user_datastore.find_user(email=current_app.config["EMAIL"]):
-        user_datastore.create_user(email=current_app.config["EMAIL"], 
-                                   password=hash_password(current_app.config["PASSWORD"]),
-                                  username=current_app.config["USERNAME"])
+    if not user_datastore.find_user(email=app.config["EMAIL"]):
+        user_datastore.create_user(email=app.config["EMAIL"], 
+                                   password=hash_password(app.config["PASSWORD"]),
+                                  username=app.config["USERNAME"])
         db.session.commit()
