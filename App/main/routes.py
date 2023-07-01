cdf09322b7ba05e80ab25e8bed4a7fda134c0ee5
Roberth <marcanoroberth98@gmail.com>
2023-07-01 18:43:44 -0300

    test endpoint

--- App/main/routes.py
+++ App/main/routes.py
@@ -1,7 +1,12 @@
 from App.main import bp
-from flask import render_template
+from flask import render_template, current_app
 from flask_security import auth_required
 from flask import request, redirect
+from App.models.auth import auth_app
+from App.extensions.db import db
+from datetime import datetime
+from App.auth.funcs import decrypt
+import request
 
 @bp.route("/")
 def index():
@@ -23,3 +28,24 @@ def webhook():
         raw = request.get_json()
         print("Notificacion recibida: ", raw)
         return "Webhook received"
+    
+@bp.route("/test")
+@auth_required("basic")
+def test():
+    last_auth = db.session.scalars(db.select(auth_app).order_by(auth_app.expire.desc())).first()
+    # Check if exists token
+    if last_auth == None:
+        return render_template("update/token_error.html")
+    diff = datetime.utcnow() - last_auth.expire
+    # The token expired
+    if diff.total_seconds()/3600 > 6:
+        return render_template("update/token_error.html")
+    
+    # Decrypt token
+    token = decrypt(last_auth.token, current_app.config["SECRET_KEY"])
+    headers = {
+            'Authorization': f'Bearer {token}'
+    }
+    url = f"https://app.multivende.com/api/d/info"
+    response = request("GET", url=url, headers=headers)
+    return response.text
